7f815b7d62577da12bc21102cedf76cb5bb1a232
joonho lee <joonfluence.dev@gmail.com>
2023-02-25 07:11:38 +0900

    [REFACTOR] 코드 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
+import About from "./pages/about";
+import Dashboard from "./pages/dashboard";
+import Home from "./pages/home";
+import NoMatch from "./pages/nomatch";
 
 function App() {
   return (
@@ -16,75 +20,3 @@ function App() {
 }
 
 export default App;
-
-const Layout = () => {
-  const Wrapper = styled.div`
-  width: 100%;
-  `;
-
-  return (
-    <Wrapper>
-      <h1>Basic Example</h1>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
-      <Outlet />
-    </Wrapper>
-  );
-};
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export default About;
