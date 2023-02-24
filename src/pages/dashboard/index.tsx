a0457b84fd50c87acf26ff4f1e12157c4e592c17
joonho lee <joonfluence.dev@gmail.com>
2023-02-25 07:11:38 +0900

    [REFACTOR] 코드 리팩토링

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/nomatch/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const NoMatch = (props: Props) => {
+  return (
+    <div>
+      <h2>NoMatch</h2>
+    </div>
+  );
+};
+
+export default NoMatch;
